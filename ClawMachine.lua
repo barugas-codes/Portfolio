43d59395f4ed0c334293e7883826bb277f5bca95
barugas-codes <ab4190153@gmail.com>
2025-03-23 17:27:51 +0000

    Update ClawMachine.lua

--- ClawMachine.lua
+++ ClawMachine.lua
@@ -8,6 +8,7 @@ currentPlayers = {}
 local goingDown = false 
 local minimumSurvivalsForClaw = 3
 
+-- If any players are welded to the claw, it will unweld them
 local function destroyWeld()
 	game.Workspace.Conveyor.Grabber.Claw.Hitbox.Script.Enabled = false
 	for _, weld in pairs(game.Workspace.Conveyor.Grabber.Claw.Hitbox:GetChildren()) do
@@ -20,6 +21,7 @@ local function destroyWeld()
 	game.Workspace.Conveyor.Grabber.Claw.Hitbox.Script.Enabled = true
 end
 
+-- A simple countdown for the round and intermission
 function module.Countdown(Time)
 	local originalTime = Time
 	
@@ -38,6 +40,7 @@ function module.Countdown(Time)
 	end
 end
 
+--Beginning the intermission
 function module.Intermission()
 	module.Countdown(30)
 end
@@ -46,11 +49,12 @@ function module.StartRound()
 	local grabber 
 	local plrsTable = plrs:GetPlayers()
 
+	--This if statement is done to check if someone already purchased the become instant grabber devproduct
 	if not plrs:FindFirstChild("Grabber", true) then
 		repeat
 			grabber = plrsTable[math.random(1, #plrsTable)]
 			wait()
-		until grabber.leaderstats.Survivals.Value > minimumSurvivalsForClaw
+		until grabber.leaderstats.Survivals.Value > minimumSurvivalsForClaw 	--The player must have a certain amount of survivals to be picked as the grabber
 
 		print(grabber.Name.." has been selected as the grabber!")
 
@@ -62,6 +66,7 @@ function module.StartRound()
 		print(grabber.Name.." has been selected as the grabber!")
 	end
 
+	--Below, the players are put into the claw machine as the round starts.
 	for _, plr in pairs(plrsTable) do
 		if plr ~= grabber then
 			plr.Character.HumanoidRootPart.Position = game.Workspace.Machine.PlayerSpawn.Position
@@ -70,11 +75,9 @@ function module.StartRound()
 			local val = Instance.new("StringValue", plr)
 			val.Name = "Competitor"
 
+			--Making the players bigger
 			local humanoid = plr.Character:FindFirstChildOfClass("Humanoid")
 			if humanoid then
-				print(humanoid.BodyHeightScale.Value, humanoid.BodyWidthScale.Value, humanoid.BodyDepthScale.Value, humanoid.HeadScale.Value)
-
-				
 				humanoid.BodyHeightScale.Value = 10
 				humanoid.BodyWidthScale.Value = 10
 				humanoid.BodyDepthScale.Value = 10
@@ -91,7 +94,7 @@ function module.StartRound()
 	round.Name = "CurrentRound"
 	round.Value = true
 	
-	module.Countdown(60)
+	module.Countdown(60) --The round now goes for 60 seconds
 end
 
 
@@ -99,10 +102,11 @@ function module.EndRound()
 	local plrsTable = plrs:GetPlayers()
 
 	for _, plr in pairs(plrsTable) do
-		plr.Character.HumanoidRootPart.Position = game.Workspace.SpawnLocation.Position + Vector3.new(0,3,0)
+		plr.Character.HumanoidRootPart.Position = game.Workspace.SpawnLocation.Position + Vector3.new(0,3,0) --Bringing players back to spawn
 		
-		destroyWeld()
+		destroyWeld() --Calling destroyweld to check if there is a player currently being held by the claw
 		
+		--Making players normal size again
 		local humanoid = plr.Character:FindFirstChildOfClass("Humanoid")
 		if humanoid then
 			humanoid.BodyHeightScale.Value = 1
@@ -112,7 +116,8 @@ function module.EndRound()
 		end
 		
 		task.wait()
-
+		
+		--Allowing the grabber to walk again
 		if plr:FindFirstChild("Grabber") then
 			if not plr:FindFirstChild("Competitor") then
 				plr.Character.Humanoid.WalkSpeed = 16
@@ -121,6 +126,7 @@ function module.EndRound()
 			end
 		end
 		
+		--Adding on survivals if the player did not get caught by the claw
 		if plr:FindFirstChild("Competitor") then
 			plr.leaderstats.Survivals.Value += 1
 			plr.Competitor:Destroy()
@@ -132,10 +138,12 @@ function module.EndRound()
 	module.Countdown(30)
 end
 
+--Full claw movement
 function module.Movement(input, isHeld)
 	print(input, isHeld)
 	local moveDirection = nil
 	
+	--Below sets the movement variable
 	if input == Enum.KeyCode.A then
 		moveDirection = Vector3.new(0, 0, 10)
 	elseif input == Enum.KeyCode.W then
@@ -151,6 +159,7 @@ function module.Movement(input, isHeld)
 		local TweenService = game:GetService("TweenService")
 		local tweenTime = 1 
 
+		--Claw moving down
 		repeat
 			for _, part in pairs(grabber:GetDescendants()) do
 				if part:IsA("BasePart") then
@@ -160,6 +169,7 @@ function module.Movement(input, isHeld)
 			task.wait(0.01)
 		until grabber.Claw.Body.Position.Y == 318
 
+		--Closing the prongs to pick up player
 		for _, prong in pairs(prongs) do
 			local targetCFrame = prong.Transparent.CFrame
 			local tweenInfo = TweenInfo.new(tweenTime, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
@@ -169,6 +179,7 @@ function module.Movement(input, isHeld)
 
 		task.wait(2) 
 
+		--Claw moving back up
 		repeat
 			for _, part in pairs(grabber:GetDescendants()) do
 				if part:IsA("BasePart") then
@@ -186,6 +197,7 @@ function module.Movement(input, isHeld)
 		local TweenService = game:GetService("TweenService")
 		local tweenTime = 1 
 
+		--Claw moving down
 		repeat
 			for _, part in pairs(grabber:GetDescendants()) do
 				if part:IsA("BasePart") then
@@ -195,6 +207,7 @@ function module.Movement(input, isHeld)
 			task.wait(0.01)
 		until grabber.Claw.Body.Position.Y == 348
 
+		--Claw opening to drop the player
 		for _, prong in pairs(prongs) do
 			local targetCFrame = prong.Original.CFrame
 			local tweenInfo = TweenInfo.new(tweenTime, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
@@ -202,10 +215,11 @@ function module.Movement(input, isHeld)
 			tween:Play()
 		end
 		
-		destroyWeld()
+		destroyWeld() --Letting go of the player
 
 		task.wait(2) 
 
+		--Moving back up
 		repeat
 			for _, part in pairs(grabber:GetDescendants()) do
 				if part:IsA("BasePart") then
@@ -218,7 +232,7 @@ function module.Movement(input, isHeld)
 		goingDown = false
 	end
 
-	if moveDirection and not goingDown then
+	if moveDirection and not goingDown then --Checking if the claw is not moving up or down, and to check if the grabber is trying to move the claw left, right, forward or backwards
 		while isHeld() do
 			
 			if goingDown then
@@ -226,28 +240,28 @@ function module.Movement(input, isHeld)
 				break
 			end	
 			
-			if input == Enum.KeyCode.A and game.Workspace.Conveyor.Grabber.Claw.Body.Position.Z < 155 then
+			if input == Enum.KeyCode.A and game.Workspace.Conveyor.Grabber.Claw.Body.Position.Z < 155 then --Stops at a certain position
 				for _, part in pairs(game.Workspace.Conveyor.Grabber:GetDescendants()) do
 					if part:IsA("BasePart") then
-						part.Position += moveDirection * 0.05
+						part.Position += moveDirection * 0.05 --Moving gradually in accordance to the direction
 					end
 				end
-			elseif input == Enum.KeyCode.D and game.Workspace.Conveyor.Grabber.Claw.Body.Position.Z > -90 then
+			elseif input == Enum.KeyCode.D and game.Workspace.Conveyor.Grabber.Claw.Body.Position.Z > -90 then --Stops at a certain position
 				for _, part in pairs(game.Workspace.Conveyor.Grabber:GetDescendants()) do
 					if part:IsA("BasePart") then
-						part.Position += moveDirection * 0.05
+						part.Position += moveDirection * 0.05 --Moving gradually in accordance to the direction
 					end
 				end
-			elseif input == Enum.KeyCode.W and game.Workspace.Conveyor.Grabber.Claw.Body.Position.X > -650 then
+			elseif input == Enum.KeyCode.W and game.Workspace.Conveyor.Grabber.Claw.Body.Position.X > -650 then --Stops at a certain position
 				for _, part in pairs(game.Workspace.Conveyor:GetDescendants()) do
 					if part:IsA("BasePart") then
-						part.Position += moveDirection * 0.05
+						part.Position += moveDirection * 0.05 --Moving gradually in accordance to the direction
 					end
 				end
-			elseif input == Enum.KeyCode.S and game.Workspace.Conveyor.Grabber.Claw.Body.Position.X < -405 then
+			elseif input == Enum.KeyCode.S and game.Workspace.Conveyor.Grabber.Claw.Body.Position.X < -405 then --Stops at a certain position
 				for _, part in pairs(game.Workspace.Conveyor:GetDescendants()) do
 					if part:IsA("BasePart") then
-						part.Position += moveDirection * 0.05
+						part.Position += moveDirection * 0.05 --Moving gradually in accordance to the direction
 					end
 				end
 			end
@@ -256,6 +270,7 @@ function module.Movement(input, isHeld)
 	end
 end
 
+-- The below function sends the players into the claw machine, making them bigger too. This is only used if a round is happening, and a player joins mid round.
 function module.AddPlayer(plr, char)
 	char.HumanoidRootPart.Position = game.Workspace.Machine.PlayerSpawn.Position
 	table.insert(currentPlayers, plr)
